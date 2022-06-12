00310b924b1a96116b58b240586a64387892ed85
NiceDeagBro <74470893+NiceDeagBro@users.noreply.github.com>
2022-06-13 00:44:41 +0300

    Add files via upload

---
+++ Warehouse Container Organizer/WareHouseProblemExtended/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WareHouseProblemExtended
+{
+    class Boxes
+    {
+        private int type;     // Type of a box
+        private int volume;   // Volume of a box
+        private int rows;     // Volume of a box
+        private int rowSize;  // Volume of a box
+
+        public Boxes(int type)
+        {
+            this.type = type;
+            switch (type)
+            {
+                case 1:
+                    volume = 1;             //volume is equal to 1 since Type 1 container is 1x1x1 meters in size
+                    rows = 17;              //amount of rows that will fit in a warehouse leaving the required space between them
+                    rowSize = 50 * 6;       
+                    break;
+                case 2:
+                    volume = 4;             //volume is equal to 4 since Type 2 container is 2x2x1 meters in size
+                    rows = 13;              //amount of rows that will fit in a warehouse leaving the required space between them
+                    rowSize = 50 * 2 * 6;
+                    break;
+                case 3:
+                    volume = 8;             //volume is equal to 8 since Type 3 container is 2x2x2 meters in size
+                    rows = 13;              //amount of rows that will fit in a warehouse leaving the required space between them
+                    rowSize = 50 * 2 * 6;   //warehouse width * container's height * height of warehouse / 2
+                    break;
+            }
+
+        }
+
+        public int warehouseVolume()
+        {
+            return rows * rowSize;
+        }
+
+
+        public double addBoxes(int containerAmount)
+        {
+            double freeSpace = this.warehouseVolume() - this.volume * containerAmount;
+            return freeSpace;
+        }
+
+        public double removeBoxes(int freeSpace, int containerAmount)
+        {
+            double freeSpaceLeft = freeSpace + this.volume * containerAmount;
+            return freeSpaceLeft;
+        }
+    }
+}
---
+++ Warehouse Container Organizer/WareHouseProblemExtended/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WareHouseProblemExtended
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int containerType;
+
+            if (radioButton1.Checked)
+            {
+                containerType = 1;
+            }
+
+            else if (radioButton2.Checked)
+            {
+                containerType = 2;
+            }
+
+            else if (radioButton3.Checked)
+            {
+                containerType = 3;
+                
+            }
+
+            else
+            {
+                textBox2.Text = "Please choose the container type.";
+                return;
+            }
+
+            int containerAmount = Int32.Parse(textBox1.Text);
+
+            Boxes box = new Boxes(containerType);
+            double calculatedValue = box.addBoxes(containerAmount);
+
+            if (calculatedValue < 0)
+            {
+                textBox2.Text = "You are trying to store too many containers";
+            }
+            else
+            {
+                textBox2.Text = calculatedValue.ToString();                         // + " m" + (char)179;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int freeSpace, containerType;
+
+            if (radioButton1.Checked)
+            {
+                containerType = 1;
+            }
+
+            else if (radioButton2.Checked)
+            {
+                containerType = 2;
+            }
+
+            else
+            {
+                containerType = 3;
+            }
+
+            int contAmount = Int32.Parse(textBox1.Text);
+            Boxes box = new Boxes(containerType);
+            freeSpace = Int32.Parse(textBox2.Text);
+            double calculatedValue = box.removeBoxes(freeSpace, contAmount);
+            //textBox2.Text = box.removeBoxes(freeSpace, contAmount).ToString();
+
+            if (calculatedValue > box.warehouseVolume())
+            {
+                textBox2.Text = "Add more containers first";
+            }
+
+            else
+            {
+                textBox2.Text = box.removeBoxes(freeSpace, contAmount).ToString();
+            }
+        }
+    }
+}
